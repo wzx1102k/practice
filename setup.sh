0e6518b95190aa2e281b0e5784deb6a633d3aeb1
cloud.yu <wzx1102k@users.noreply.github.com>
2017-10-01 11:12:32 +0800

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+apt-get install build-essential cmake git pkg-config
+apt-get install libjpeg8-dev libtiff4-dev libjasper-dev libpng12-dev
+apt-get install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
+apt-get install libgtk2.0-dev
+apt-get install libatlas-base-dev gfortran
+apt-get install python3
+apt-get install pip3
+apt-get install python3-tk
